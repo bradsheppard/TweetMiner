a1a90e9eb51413e6151d6af1fd184f2855ef1e58
Brad Sheppard <bradsheppard22@gmail.com>
2018-04-08 23:53:15 -0230

    Fixed title bar size for mobile.

--- components/top.js
+++ components/top.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
-
+import Hidden from 'material-ui/Hidden';
 import { withStyles } from 'material-ui/styles';
 
 const style = {
     typography: {
         paddingBottom: '1em',
-        paddingTop: '1em'
-    },
-    grid: {
+        paddingTop: '1em',
         backgroundColor: '#ffffff'
     }
 };
@@ -18,17 +15,18 @@ function Top (props) {
     const { classes } = props;
 
     return(
-        <Grid container
-              className={classes.grid}
-              alignItems='center'
-              direction='row'
-              justify='center'>
-            <Grid xs={4} item>
-                <Typography className={classes.typography} variant="display4">
+        <div>
+            <Hidden implementation='css' xsDown>
+                <Typography className={classes.typography} variant="display4" align='center'>
                     Tweet Miner
                 </Typography>
-            </Grid>
-        </Grid>
+            </Hidden>
+            <Hidden implementation='css' smUp>
+                <Typography className={classes.typography} variant="display3" align='center'>
+                    Tweet Miner
+                </Typography>
+            </Hidden>
+        </div>
     );
 }
 
